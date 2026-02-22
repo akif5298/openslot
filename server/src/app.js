3cfae5a9f0c86eae73a23c048e74139918a3f6f0
Dharmik <73265503+Dharmik1411@users.noreply.github.com>
2026-02-22 13:33:36 -0500

    ....

--- server/src/app.js
+++ server/src/app.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use("/api", routes);
 
 app.get("/", (req, res) => {
-  res.json({ ok: true, service: "OpenSlot API", milestone: 1 });
+  res.json({ ok: true, service: "OpenSlot API", milestone: 2 });
 });
 
 app.listen(PORT, () => {
